de4b586676b22c10cdf43b749b40fa09a2e9c934
TunahanKaracoban <77543156+TunahanKaracoban@users.noreply.github.com>
2022-01-05 16:58:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
